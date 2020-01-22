365f9a9414402d25105636e3e2dcc8f778b8f265
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-01-22 15:48:42 -0600

    part 1 complete with zombie

--- hero_rpg.py
+++ hero_rpg.py
@@ -5,7 +5,6 @@
 # 1. fight goblin
 # 2. do nothing - in which case the goblin will attack him anyway
 # 3. flee
-
 class Character: 
     def __init__(self, name, health, power):
         self.name = name
@@ -30,6 +29,11 @@ class Hero(Character):
 class Goblin(Character):
     pass
 
+class Zombie(Character):
+    def alive(self):
+        return True
+
+
 def main():
     hero = Hero("Carl", 10, 5)
     goblin = Goblin("General Wartface", 6, 2)
@@ -50,14 +54,45 @@ def main():
         elif raw_input == "2":
             pass
         elif raw_input == "3":
-            print(f".")
+            print(f"{goblin.name} laughs.")
             break
         else:
             print("Invalid input {}".format(raw_input))
 
-        if goblin.health > 0:
+        if goblin.alive():
             # Goblin attacks hero
             goblin.attack(hero)
 
-main()
+# main()
+
+def main2():
+    hero = Hero("Carl", 10, 5)
+    zombie = Zombie("Rick Grimes", 1, 1)
+
+    while zombie.alive() and hero.alive():
+        hero.print_status()
+        zombie.print_status()
+        print()
+        print("What do you want to do?")
+        print("1. fight zombie")
+        print("2. do nothing")
+        print("3. flee")
+        print("> ", end=' ')
+        raw_input = input()
+        if raw_input == "1":
+            # Hero attacks goblin
+            hero.attack(zombie)
+        elif raw_input == "2":
+            pass
+        elif raw_input == "3":
+            print(f"{zombie.name} laughs.")
+            break
+        else:
+            print("Invalid input {}".format(raw_input))
+
+        if zombie.alive():
+            # Goblin attacks hero
+            zombie.attack(hero)
+
+main2()
 
