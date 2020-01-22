7c252005cc9655ffb774ebcbc392358043614737
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-01-22 15:10:57 -0600

    finished part 1

--- hero_rpg.py
+++ hero_rpg.py
@@ -11,40 +11,32 @@ class Character:
         self.name = name
         self.health = health
         self.power = power
+    def attack(self, enemy):
+        enemy.health -= self.power
+        print(f"{self.name} does {self.power} damage to the {enemy.name}.")
+        if enemy.health <= 0:
+            print(f"{enemy.name} is dead.")
+    def alive(self):
+        if self.health <= 0:
+            return False
+        else: 
+            return True
+    def print_status(self):
+        print(f"{self.name} has {self.health} health and {self.power} power.")
 
 class Hero(Character):
-    def attack(self, enemy):
-        enemy.health -= self.power
-        print("You do {} damage to the goblin.".format(self.power))
-        if enemy.health <= 0:
-            print("The goblin is dead.")
-    def alive(self):
-        if self.health <= 0:
-            return False
-        else: 
-            return True
+    pass
 
 class Goblin(Character):
-    def attack(self, enemy):
-        enemy.health -= self.power
-        print("The goblin does {} damage to you.".format(self.power))
-        if enemy.health <= 0:
-            print("You are dead.")
-    def alive(self):
-        if self.health <= 0:
-            return False
-        else: 
-            return True
-
-
+    pass
 
 def main():
     hero = Hero("Carl", 10, 5)
     goblin = Goblin("General Wartface", 6, 2)
 
     while goblin.alive() and hero.alive():
-        print("You have {} health and {} power.".format(hero.health, hero.power))
-        print("The goblin has {} health and {} power.".format(goblin.health, goblin.power))
+        hero.print_status()
+        goblin.print_status()
         print()
         print("What do you want to do?")
         print("1. fight goblin")
@@ -58,7 +50,7 @@ def main():
         elif raw_input == "2":
             pass
         elif raw_input == "3":
-            print("Goodbye.")
+            print(f".")
             break
         else:
             print("Invalid input {}".format(raw_input))
