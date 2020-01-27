d5504cdb3055f0e49f7651f5adb6875b073e99f9
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-01-27 14:37:26 -0600

    Completed with loading times.

--- hero_rpg.py
+++ hero_rpg.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import random
+import asci_art
+import time
 
 line_spacing = "====================="
 
@@ -22,7 +24,9 @@ class Character:
         else: 
             self.damage = self.power - enemy.armor
             enemy.health -= self.damage
+            time.sleep(1.5)
             print(f"{self.name} does {self.damage} damage to the {enemy.name}.")
+            time.sleep(1.5)
         if enemy.health <= 0:
             print(f"{enemy.name} is dead!")
             quit()
@@ -39,17 +43,21 @@ class Hero(Character):
     def attack(self, enemy):
         #Crit value is a multiplier of power and doubles 20% of the time
         self.damage = (self.power * self.crit_multiplier()) - enemy.armor
+        time.sleep(1.5)
         print(f"{self.name} does {self.damage} damage to the {enemy.name}.")
         enemy.health -= self.damage
         if not enemy.alive():
+            time.sleep(1.5)
             print(f"{enemy.name} is dead!")
             self.coins += enemy.coins
+            time.sleep(1.5)
             print(f"{self.name} has received the bounty for {enemy.name} of {enemy.coins}.")
-            print(line_spacing)
+            time.sleep(1.5)
     def crit_multiplier(self):
         self.crit_mult = 1
         if random.randint(1, 6) == 3:
             self.crit_mult = 2
+            time.sleep(0.5)
             print(f"{self.name} has dealt a critical strike!")
         return self.crit_mult
     def buy(self, item):
@@ -57,28 +65,35 @@ class Hero(Character):
             self.coins -= item.cost
             self.inventory.append(item)
             item.count += 1
+            time.sleep(1.5)
             print(f"{item.name} added to {self.name}\'s inventory.")
         else: 
+            time.sleep(1.5)
             print("You do not have enough coins to purchase this item.")
     def use_item(self):
         if len(self.inventory) > 0:
-            print(line_spacing)
+            time.sleep(1.5)
+            print("*" + "-" * 60 + "*")
             print("Which item do you wish to use?")
             for i in range(len(self.inventory)):
                 item = self.inventory[i]
                 print("{}. use {} ({})".format(i + 1, item.name, item.count))
             print(str((len(self.inventory) + 1)) + ". leave")
             raw_imp = int(input("> "))
+            time.sleep(1.5)
             if raw_imp == len(self.inventory) + 1:
-                print(line_spacing)
+                print("*" + "-" * 60 + "*")
             elif raw_imp > len(self.inventory) + 1:
+                time.sleep(1.5)
                 print("Invalid input.")
             else:
                 ItemToUse = self.inventory[raw_imp - 1]
                 item = ItemToUse
+                time.sleep(1.5)
                 item.apply(self)
                 self.inventory.remove(item)
         else: 
+            time.sleep(1.5)
             print(f"{self.name} has no items.")
 
 
@@ -146,6 +161,7 @@ class Sword:
     def __init__(self):
         self.cost = 10
         self.name = "sword"
+        self.count = 0
     def apply(self,character):
         character.power += 5
         print(f"{character.name}\'s power has been increased to {character.power}.")
@@ -159,18 +175,24 @@ class Store:
     items = [tonic, armor, evade, sword]
     def do_shopping(self, hero):
         while True:
-            print(line_spacing)
-            print("Welcome to the store!")
-            print(line_spacing)
+            time.sleep(1.5)
+            print("*" + "-" * 60 + "*")
+            the_store = "The Store"
+            print(the_store.center(62))
+            print("*" + "-" * 60 + "*")
+            asci_art.shop_art()
+            print("*" + "-" * 60 + "*")
             print("You have {} coins.".format(hero.coins))
             print("What do you want to do?")
             for i in range(len(self.items)):
                 item = self.items[i]
                 print("{}. buy {} ({})".format(i + 1, item.name, item.cost))
             print("5. leave")
+            print("*" + "-" * 72 + "*")
             raw_imp = int(input("> "))
+            print("*" + "-" * 72 + "*")
             if raw_imp == 5:
-                print(line_spacing)
+                time.sleep(1.5)
                 break
             elif raw_imp > 5:
                 print("Invalid input.")
@@ -179,13 +201,18 @@ class Store:
                 item = ItemToBuy
                 hero.buy(item)
     def go_shopping(self, character):
+        print("*" + "-" * 72 + "*")
+        asci_art.road_art()
+        print("*" + "-" * 72 + "*")
         print("1. Enter the store.")
         print("Press another number to battle.")
+        print("*" + "-" * 72 + "*")
         store_status = int(input("> "))
         if store_status == 1:
+            time.sleep(1.5)
             self.do_shopping(character)
         else: 
-            print(line_spacing)
+            time.sleep(1.5)
 
 
 def main():
@@ -197,28 +224,31 @@ def main():
     knight = Knight("Sir Gawayne the Handsome", 10, 4, 10)
     yogi = Yogi("Ravi Shankar", 1, 1, 0)
     store = Store()
-    print()
-    print(f"{goblin.name} approaches.")
 
     while goblin.alive() and hero.alive():
+        print("*" + "-" * 60 + "*")
+        print(goblin.name.center(62))
+        print("*" + "-" * 60 + "*")
+        asci_art.goblin_art()
+        print("*" + "-" * 60 + "*")
         hero.print_status()
         goblin.print_status()
-        print()
+        print("*" + "-" * 60 + "*")
         print("What do you want to do?")
         print(f"1. fight {goblin.name}")
         print("2. do nothing")
         print("3. flee")
         print("4. access inventory")
+        print("*" + "-" * 60 + "*")
         print("> ", end=' ')
         raw_input = input()
-        print(line_spacing)
-        print()
         if raw_input == "1":
             # Hero attacks goblin
             hero.attack(goblin)
         elif raw_input == "2":
             pass
         elif raw_input == "3":
+            time.sleep(1.5)
             print(f"{goblin.name} laughs.")
             break
         elif raw_input == "4":
@@ -232,21 +262,23 @@ def main():
         
     store.go_shopping(hero)
 
-    print()
-    print(f"{zombie.name} approaches.")
-
     while zombie.alive() and hero.alive():
+        print("*" + "-" * 60 + "*")
+        print(zombie.name.center(62))
+        print("*" + "-" * 60 + "*")
+        asci_art.zombie_art()
+        print("*" + "-" * 60 + "*")
         hero.print_status()
         zombie.print_status()
-        print()
+        print("*" + "-" * 60 + "*")
         print("What do you want to do?")
         print(f"1. fight {zombie.name}")
         print("2. do nothing")
         print("3. flee")
         print("4. access inventory")
+        print("*" + "-" * 60 + "*")
         print("> ", end=' ')
         raw_input = input()
-        print(line_spacing)
         if raw_input == "1":
             # Hero attacks zombie
             hero.attack(zombie)
@@ -266,21 +298,23 @@ def main():
     
     store.go_shopping(hero)
     
-    print()
-    print(f"{medic.name} approaches.")
-    
     while medic.alive() and hero.alive():
+        print("*" + "-" * 60 + "*")
+        print(medic.name.center(62))
+        print("*" + "-" * 60 + "*")
+        asci_art.medic_art()
+        print("*" + "-" * 60 + "*")
         hero.print_status()
         medic.print_status()
-        print()
+        print("*" + "-" * 60 + "*")
         print("What do you want to do?")
         print(f"1. fight {medic.name}")
         print("2. do nothing")
         print("3. flee")
         print("4. access inventory")
+        print("*" + "-" * 60 + "*")
         print("> ", end=' ')
         raw_input = input()
-        print(line_spacing)
         if raw_input == "1":
             # Hero attacks medic
             hero.attack(medic)
@@ -301,26 +335,29 @@ def main():
     
     store.go_shopping(hero)
     
-    print()
-    print(f"{ghost.name} approaches.")
-    
     while ghost.alive() and hero.alive():
+        print("*" + "-" * 60 + "*")
+        print(ghost.name.center(62))
+        print("*" + "-" * 60 + "*")
+        asci_art.ghost_art()
+        print("*" + "-" * 60 + "*")
         hero.print_status()
         ghost.print_status()
-        print()
+        print("*" + "-" * 60 + "*")
         print("What do you want to do?")
         print(f"1. fight {ghost.name}")
         print("2. do nothing")
         print("3. flee")
         print("4. access inventory")
+        print("*" + "-" * 60 + "*")
         print("> ", end=' ')
         raw_input = input()
-        print(line_spacing)
         if raw_input == "1":
             # Hero attacks ghost
             if random.randint(1, 11) == 2:
                 hero.attack(ghost)
             else:
+                time.sleep(1.5)
                 print(f"{ghost.name} has dodged {hero.name}\'s attack!")
 
         elif raw_input == "2":
@@ -339,21 +376,24 @@ def main():
         
     store.go_shopping(hero)
     
-    print()
-    print(f"{knight.name} approaches.")
-    
     while knight.alive() and hero.alive():
+        print("*" + "-" * 60 + "*")
+        print(knight.name.center(62))
+        print("*" + "-" * 60 + "*")
+        asci_art.knight_art()
+        print("*" + "-" * 60 + "*")
         hero.print_status()
         knight.print_status()
-        print()
+        knight.dialogue(hero)
+        print("*" + "-" * 60 + "*")
         print("What do you want to do?")
         print(f"1. fight {knight.name}")
         print("2. do nothing")
         print("3. flee")
         print("4. access inventory")
+        print("*" + "-" * 60 + "*")
         print("> ", end=' ')
         raw_input = input()
-        print(line_spacing)
         if raw_input == "1":
             # Hero attacks knight
             hero.attack(knight)
@@ -370,34 +410,35 @@ def main():
 
         if knight.alive():
             # Knight attacks hero
-            print()
-            knight.dialogue(hero)
-            print()
             knight.attack(hero)
     
     store.go_shopping(hero)
     
-    print()
-    print(f"{yogi.name} approaches.")
-    
     while yogi.alive() and hero.alive():
+        print("*" + "-" * 60 + "*")
+        print(yogi.name.center(62))
+        print("*" + "-" * 60 + "*")
+        asci_art.yogi_art()
+        print("*" + "-" * 60 + "*")
         hero.print_status()
         yogi.print_status()
-        print()
+        print("*" + "-" * 60 + "*")
         print("What do you want to do?")
         print(f"1. fight {yogi.name}")
         print("2. do nothing")
         print("3. flee")
         print("4. access inventory")
+        print("*" + "-" * 60 + "*")
         print("> ", end=' ')
         raw_input = input()
-        print(line_spacing)
         if raw_input == "1":
             # Hero attacks yogi but yogi forces hero to flee
+            time.sleep(2)
             print("Violence is not the answer.")
             break
         elif raw_input == "2":
             #yogi blesses the hero
+            time.sleep(2)
             print()
             print("I bless you with my powers.")
             hero.health += 5
@@ -405,7 +446,9 @@ def main():
             hero.print_status()
             break
         elif raw_input == "3":
+            time.sleep(2)
             print(f"{yogi.name} waves goodbye in a Downward Dog.")
+            time.sleep(1)
             break
         elif raw_input == "4":
             hero.use_item()
